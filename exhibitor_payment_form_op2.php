2c4ab278136b2f81cb9cd77ecb4aae7a22966db5
Rohit-Lakheda <lakhedarohit@gmail.com>
2025-11-08 11:08:50 +0530

    update error display

--- exhibitor_payment_form_op2.php
+++ exhibitor_payment_form_op2.php
@@ -2,10 +2,7 @@
 
 session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-//print_r($_POST);exit;
+
 
 if (empty($_SESSION["vercode_ex"]) || ($_POST["vercode_ex"] != $_SESSION["vercode_ex"])) {
 
