4f8ba7397684c84a9c5d8e60217c8c8a602804d0
Manish Kumar Sharma <manishk_sharma@outlook.com>
2025-11-08 11:09:05 +0530

    display hidden

--- exhibitor_payment_form_op2.php
+++ exhibitor_payment_form_op2.php
@@ -2,8 +2,8 @@
 
 session_start();
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
 error_reporting(E_ALL);
 //print_r($_POST);exit;
 
